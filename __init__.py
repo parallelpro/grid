4fcb860b52d9a185f8476067369b808f27b06201
Yaguang Li <hnwilliam@hotmail.com>
2022-04-07 20:18:52 +1000

    support importing as a package

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
-from modelling import grid, get_surface_correction, get_model_Dnu, get_obs_Dnu
+from .modelling import grid, get_surface_correction, get_model_Dnu, get_obs_Dnu
 
 __all__ = ['get_surface_correction', 'grid', 'get_model_Dnu', 'get_obs_Dnu']
