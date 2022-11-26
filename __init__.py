9fed6172a6f31278be0d77ecb888f348c69e4fec
yaguang li <parallelpro@users.noreply.github.com>
2022-11-26 20:14:50 +1100

    Made "match_modes" routine importable.

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
-from .src import grid, get_surface_correction, get_model_Dnu, get_obs_Dnu
+from .src import grid, get_surface_correction, get_model_Dnu, get_obs_Dnu, match_modes
 
-__all__ = ['get_surface_correction', 'grid', 'get_model_Dnu', 'get_obs_Dnu']
+__all__ = ['get_surface_correction', 'grid', 'get_model_Dnu', 'get_obs_Dnu', 'match_modes']
